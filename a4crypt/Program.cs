a773a7af2c54882a7b1b71e02e32d106c167a6c3
a4barros <antenor@a4barros.com>
2026-03-08 19:31:52 -0300

    setting core as a lib

--- a4crypt/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using a4crypt;
-
-
-A4CryptCore.Encrypt("G:\\Documents\\drone.png", "G:\\Documents\\drone.a4cr", "123");
-A4CryptCore.Decrypt("G:\\Documents\\drone.a4cr", "G:\\Documents\\a.png", "123");
